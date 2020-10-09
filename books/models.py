da5981d44ec6759e3b3d7a1c0f8e1c0eafd29b60
Jimiliani <dikorolyov@mail.ru>
2020-10-09 14:57:18 +0300

    Добавил модели книги и автора

---
+++ books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField()
+    price = models.IntegerField(null=False, blank=False)
+    authors = models.ManyToManyField('Author', on_delete=models.PROTECT, null=False, blank=False)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    about = models.TextField()
